833a38bbc66c41db1987986a90a8a5ea873c6632
Ali M.Mansoory K <alimansoory@yahoo.com>
2018-04-07 19:57:09 +0430

    Scroll controll added

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
 var socket = io();
 
+function scrollToBottom () {
+  var messages = $('#messages');
+  var newMessage = messages.children('li:last-child');
+
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  };
+};
 socket.on('connect', function () {
   console.log('connected to server');
 });
@@ -13,6 +27,7 @@ socket.on('newMessage', function (data) {
    createdAt: formattedTime
  });
  $('#messages').append(html);
+ scrollToBottom();
 
   // var li = $('<li></li>');
   // li.text(`${data.from} ${formattedTime}: ${data.text}`);
